35a67ecd0a02261afa87ba03b5fee0cb133f4758
Nguyen Hoang Anh Tu <quoctruong2406@gmail.com>
2018-05-01 10:47:52 +0700

    Them form LichSu_form

---
+++ QuanLyKhachSan/QuanLyKhachSan/LichSu_form.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace QuanLyKhachSan
+{
+    partial class LichSu_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.reflectionLabel1 = new DevComponents.DotNetBar.Controls.ReflectionLabel();
+            this.dgvLichSu = new DevComponents.DotNetBar.Controls.DataGridViewX();
+            this.TENPHONG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TENKH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HOTEN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NGAYNHAN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NGAYTRA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TRANGTHAI = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLichSu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reflectionLabel1
+            // 
+            // 
+            // 
+            // 
+            this.reflectionLabel1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.reflectionLabel1.Location = new System.Drawing.Point(254, 12);
+            this.reflectionLabel1.Name = "reflectionLabel1";
+            this.reflectionLabel1.Size = new System.Drawing.Size(293, 46);
+            this.reflectionLabel1.TabIndex = 3;
+            this.reflectionLabel1.Text = "<b><font size=\"+6\"><i>Lịch sử phòng</i><font color=\"#B02B2C\"> GONGCHA Hotel</font" +
+    "></font></b>";
+            // 
+            // dgvLichSu
+            // 
+            this.dgvLichSu.AllowUserToAddRows = false;
+            this.dgvLichSu.AllowUserToDeleteRows = false;
+            this.dgvLichSu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLichSu.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TENPHONG,
+            this.TENKH,
+            this.HOTEN,
+            this.NGAYNHAN,
+            this.NGAYTRA,
+            this.TRANGTHAI});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvLichSu.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvLichSu.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(170)))), ((int)(((byte)(170)))));
+            this.dgvLichSu.Location = new System.Drawing.Point(0, 64);
+            this.dgvLichSu.Name = "dgvLichSu";
+            this.dgvLichSu.ReadOnly = true;
+            this.dgvLichSu.Size = new System.Drawing.Size(759, 381);
+            this.dgvLichSu.TabIndex = 4;
+            // 
+            // TENPHONG
+            // 
+            this.TENPHONG.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.TENPHONG.DataPropertyName = "TENPHONG";
+            this.TENPHONG.HeaderText = "Tên phòng";
+            this.TENPHONG.Name = "TENPHONG";
+            this.TENPHONG.ReadOnly = true;
+            // 
+            // TENKH
+            // 
+            this.TENKH.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.TENKH.DataPropertyName = "TENKH";
+            this.TENKH.HeaderText = "Tên khách hàng";
+            this.TENKH.Name = "TENKH";
+            this.TENKH.ReadOnly = true;
+            // 
+            // HOTEN
+            // 
+            this.HOTEN.DataPropertyName = "HOTEN";
+            this.HOTEN.HeaderText = "Nhân viên";
+            this.HOTEN.Name = "HOTEN";
+            this.HOTEN.ReadOnly = true;
+            // 
+            // NGAYNHAN
+            // 
+            this.NGAYNHAN.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.NGAYNHAN.DataPropertyName = "NGAYNHAN";
+            this.NGAYNHAN.HeaderText = "Ngày nhận";
+            this.NGAYNHAN.Name = "NGAYNHAN";
+            this.NGAYNHAN.ReadOnly = true;
+            // 
+            // NGAYTRA
+            // 
+            this.NGAYTRA.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.NGAYTRA.DataPropertyName = "NGAYTRA";
+            this.NGAYTRA.HeaderText = "Ngày trả";
+            this.NGAYTRA.Name = "NGAYTRA";
+            this.NGAYTRA.ReadOnly = true;
+            // 
+            // TRANGTHAI
+            // 
+            this.TRANGTHAI.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.TRANGTHAI.DataPropertyName = "TRANGTHAI";
+            this.TRANGTHAI.HeaderText = "Trạng thái";
+            this.TRANGTHAI.Name = "TRANGTHAI";
+            this.TRANGTHAI.ReadOnly = true;
+            // 
+            // LichSu_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 457);
+            this.Controls.Add(this.dgvLichSu);
+            this.Controls.Add(this.reflectionLabel1);
+            this.Name = "LichSu_form";
+            this.Text = "LichSu_form";
+            this.Load += new System.EventHandler(this.LichSu_form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLichSu)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.Controls.ReflectionLabel reflectionLabel1;
+        private DevComponents.DotNetBar.Controls.DataGridViewX dgvLichSu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TENPHONG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TENKH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HOTEN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NGAYNHAN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NGAYTRA;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TRANGTHAI;
+    }
+}
---
+++ QuanLyKhachSan/QuanLyKhachSan/LichSu_form.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan
+{
+    public partial class LichSu_form : Form
+    {
+        public LichSu_form()
+        {
+            InitializeComponent();
+        }
+
+        QuanLyKSDataContext db = new QuanLyKSDataContext();
+
+        private void loadDSLichSu()
+        {
+            dgvLichSu.DataSource = db.LICHSU_VIEWs.Select(p => new
+            {
+                p.TENPHONG,
+                p.TENKH,
+                p.HOTEN,
+                p.NGAYNHAN,
+                p.NGAYTRA,
+                p.TRANGTHAI
+            });
+        }
+
+        private void LichSu_form_Load(object sender, EventArgs e)
+        {
+            loadDSLichSu();
+        }
+    }
+}
